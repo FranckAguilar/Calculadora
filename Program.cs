177f1d09acfef660add587d111cebdf7f1f7aaa1
Jchs2005 <1614975@senati.pe>
2025-06-19 12:38:48 -0500

    Codigo base

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Numero 1");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Numero 2");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Selecciona: +, -, *, /");
+string opcion = Console.ReadLine();
+
+double resultado = 0;
+
+// Escribir codigo aqui
+
+Console.WriteLine("Resultado: " + resultado);
